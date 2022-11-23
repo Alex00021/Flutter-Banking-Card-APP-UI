d901a4167eddf7526f7d322e1b5515e3b88f8aee
AlexLingfan <AlexLingfan@163.com>
2022-11-23 21:11:32 +0800

    1123

---
+++ flutter_banking_app_ui/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'index_page_app.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: IndexPageApp(),
+  ));
+}
+
+
